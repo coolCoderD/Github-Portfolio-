08f21cc426cd46bf010c652d9c8781c089b94c44
coolCoderD <animeeditsbyme123@gmail.com>
2024-11-30 22:19:45 +0530

    design updated

--- src/components/ui/Background.tsx
+++ src/components/ui/Background.tsx
@@ -8,11 +8,11 @@ import React from 'react'
 
 const BackgroundBeamsWithCollision = () => {
   return (
-<div className="bg-transparent h-[100vh] flex items-center justify-center px-16">
-  <div className="relative w-full max-w-lg">
-    <div className="absolute top-0 -left-4 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute top-0 -right-4 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-8 left-20 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+<div className="bg-transparent h-[100vh] fixed inset-0 flex items-center justify-center px-16">
+  <div className="relative w-full  max-w-lg">
+    <div className="absolute top-0 -left-4 w-[600px] h-[600px] bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+
+    <div className="absolute -bottom-8 left-20 w-[600px] h-[600px] bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
 
   </div>
 </div>
