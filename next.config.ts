52262d5d05b7815d11e5a412379069f500006eb2
coolCoderD <animeeditsbyme123@gmail.com>
2024-12-03 00:53:39 +0530

    error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ["res.cloudinary.com"], // Add any additional domains as needed
+    domains: ["res.cloudinary.com"],
   },
 };
 
